8e1c6fce4ee889652c4a31feedb0a6e1ee837742
Taylor pereira gomes da costa <taylorgomesdacosta@gmail.com>
2022-03-10 08:41:37 -0300

    finalizando desafio do discord

---
+++ DesafiosJavaDIscord.java
@@ -0,0 +1,10 @@
+
+public class DesafiosJavaDIscord {
+	public static void main(String[] args) {
+		System.out.println("Nome: Taylor \n" + "Idade: 33 \n" + "Trabalho: Engenheiro civil  \n" 
+	+ "Quero ser: Especialista em Java \n"+ "Estou estudando: Engenharia de Software!! ");
+		
+		
+	}
+
+}
